dce0e98f8051583faa0fb24c6cad1c773133ee15
alexianatenin.ans@gmail.com <alexianatenin.ans@gmail.com>
2024-04-19 13:06:20 +0000

    Edited LLMcomms

--- server_code/LLMcomms.py
+++ server_code/LLMcomms.py
@@ -5,19 +5,14 @@ import requests
 def get_chat_history():
   if "chat_history" not in anvil.server.session:
       anvil.server.session["chat_history"] = {
-          "past_user_inputs": ["Merry Christmas!"],
-          "llm_outputs": ["Welcome"]
+          "llm_outputs": ["Welcome to RBT, do you want to see the demo? Say No if you would rather submit instructions yourself."],
+          "past_user_inputs": [""]
       }
   return anvil.server.session["chat_history"]
 
 @anvil.server.callable
 def send_message(message):
   chat_history = get_chat_history()
-  model_output = query({
-    "inputs": {
-      "text": message,
-      **chat_history
-    }
-  })
+  model_output = 
   anvil.server.session["chat_history"] = model_output["chat_history"]
   return model_output["chat_history"]
